a224e609228eca0114e053b7fe4015aa94470a1b
JacobPoole <Jacob@ucrwpa1-7-10-25-27-194.wnet.ucr.edu>
2016-11-15 22:51:22 -0800

    CPP File update

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
+<<<<<<< Updated upstream
 #include "../headerse/parser.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <iostream>
+=======
+#include "../header/parser.h"
+#include <stdio.h>
+#include <unistd.h>
+>>>>>>> Stashed changes
 
 using namespace std;
 
 int main()
 {
+<<<<<<< Updated upstream
 	//get host name and user id
 	string cmd;
 	string user_id;
@@ -46,4 +53,35 @@ int main()
 	}
 
 	return 0;
+=======
+    Parser* cmds = new Parser();
+    string command;     //Used for user input
+    string login;       //Get the login name of the user
+    bool previous = true;
+    if (getlogin() == NULL)
+    {
+        login = "";
+        perror("get login failed"); //getlogin() failed
+    }
+    else
+    {
+        login = getlogin();         //getlogin() success
+    }
+    char host[100];                     //Get the user's hostname
+    if (gethostname(host, 100) == -1)
+    {
+        perror("get host name failed"); //gethostname() failed
+    }
+    while (true)                        //Loop until exit called
+    {
+        if (getlogin() != NULL)     //Just display "$ " if getlogin() failed
+        {
+            cout << login << "@" << host;
+        }
+        cout << "$ ";
+        getline(cin, command);      //Get user input
+        cmds->parse(command, previous);
+    }
+    return 0;
+>>>>>>> Stashed changes
 }
