5a269ae0bd083fba8fb0b9aa16ed4ee87561c579
JacobPoole <Jacob@ucrwpa4-7-120-169.wnet.ucr.edu>
2016-11-18 22:35:52 -0800

    Upload to Master

---
+++ headers/and.h
@@ -0,0 +1,23 @@
+#ifndef AND_H
+#define AND_H
+
+#include "base.h"
+#include "test.h"
+#include <boost/tokenizer.hpp>
+
+using namespace std;
+
+class And : public Base 
+{
+    private:
+        Base* cmd;
+    public:
+        And(Base* temp) 
+        {
+            cmd = temp;
+        }
+        void run(string command, bool &previous);
+};
+
+#endif
+
